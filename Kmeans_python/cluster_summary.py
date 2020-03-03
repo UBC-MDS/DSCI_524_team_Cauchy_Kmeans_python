85de1e5915d53764a680c51df01d27b1b869d592
Rob Blumberg <robertblumberg97@gmail.com>
2020-03-02 20:37:42 -0800

    Adding first version of body of cluster_summary function

--- Kmeans_python/cluster_summary.py
+++ Kmeans_python/cluster_summary.py
@@ -1,10 +1,12 @@
-def cluster_summary(centroids, cluster_assignments):
+def cluster_summary(X, centroids, cluster_assignments):
     """
     Provides summary of groups created from Kmeans clustering, including centroid coordinates,
     number of data points in training data assigned to each cluster, and within-cluster distance metrics.
     
     Parameters
     ----------
+    X : array-like, shape=(n_samples, n_features)
+        data on which Kmeans was fit
     centroids : numpy.ndarray
         N-dimensional array containing cluster center locations
     cluster_assignments : array-like
@@ -26,7 +28,23 @@ def cluster_summary(centroids, cluster_assignments):
     >>> X = np.array([[1, 2], [1, 4], [1, 0],
     ...               [10, 2], [10, 4], [10, 0]])
     >>> centers, cluster_ass = fit(X, 2)
-    >>> cluster_summary(centers, cluster_ass,)
+    >>> cluster_summary(centers, cluster_ass)
     """
+    
+    df_dict = {}
+    for i in range(centroids.shape[1]):
+        df_dict[f"x{i+1}"] = centroids[:, i]
+    
+    num_assigned_list = np.zeros(centroids.shape[1])
+    inertia_list = np.zeros(centroids.shape[1])
+    for i in np.unique(cluster_ass):
+        num_assigned_list[i] = sum(cluster_ass == i)
+        inertia_list[i] = np.sum((X[cluster_ass == i] - centroids[i])**2)
 
-    pass
+    df_dict["Number of assigned training points"] = num_assigned_list
+    df_dict["Within cluster inertia"] = inertia_list    
+    
+    summary_df = pd.DataFrame(data = df_dict)
+    summary_df.index.name = "centroid"
+     
+    return summary_df
