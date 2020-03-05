e95f0eeb5be95f96069671eee3eeb81a408da87a
jamesh4 <jamesh4@alumni.ubc.ca>
2020-03-04 21:54:24 -0800

    implemented silhouette, created test file

--- Kmeans_python/silhouette.py
+++ Kmeans_python/silhouette.py
@@ -1,3 +1,9 @@
+import altair as alt
+import numpy as np
+import pandas as pd
+from sklearn.metrics import silhouette_score
+alt.renderers.enable('notebook')
+
 def silhouette(X, k_array):
     """
     Plots a graph of silhouette scores for each k value
@@ -15,6 +21,9 @@ def silhouette(X, k_array):
     -------
     1-d array
     An array containing silhouette scores in the same order as k_array.
+    
+    Altair chart object
+    An Altair chart displaying silhouette scores with their corresponding k values.
 
     Examples
     --------
@@ -25,4 +34,18 @@ def silhouette(X, k_array):
     
     """
 
-    pass
+    scores = []
+    for i in range(len(k_array)):
+        centers, labels = fit(k_array[i])
+        score = silhouette_score(X, labels)
+        scores.append([k_array[i], score])
+        
+    scores = pd.DataFrame(scores)
+    scores.rename(columns = {0: "k", 1:"Score"}, inplace = True)
+
+    chart = (alt.Chart(scores).mark_line().encode(
+        alt.X('k:O', axis = alt.Axis(title = 'k')),
+        alt.Y('Score:Q', axis = alt.Axis(title = 'Silhouette score')), 
+    ).properties(title = "Silhouette scores", width = 800))
+    
+    return (scores["Score"], chart)
