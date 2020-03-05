a7c26595b9ac563a445f027b8a425c819a18d250
Sreejith <sreejimunthikodu@yahoo.com>
2020-03-05 01:35:46 -0800

    Wrote test cases for test_elbow.py

--- tests/test_elbow.py
+++ tests/test_elbow.py
@@ -10,3 +10,10 @@ def test_elbow():
     X = np.array([[1, 2], [1, 4], [1, 0],
         [10, 2], [10, 4], [10, 0]])
     centers = [2, 3, 4, 5]
+
+    # Test cases
+    p, inertia = elbow(X, centers)
+    assert(((isinstance(inertia, list)) | (isinstance(inertia, np.ndarray)))), "Inertia is of wrong data type"
+    assert(len(inertia) == len(centers)), "Inertia has wrong number of values"
+    assert(isinstance(p, alt.vegalite.v3.api.Chart)), "Altair plot object is not returned"
+    assert(all(y>=0 for y in inertia)), "Inertia values should be greater than or equal to zero"
